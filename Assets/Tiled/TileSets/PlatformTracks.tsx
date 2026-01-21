801e5db87ad896e4afc0415341882e5ddf3aed45
Sean Alf <no-email>
2026-01-20 21:25:57 -0800

    update platform tracks canvas size

--- Assets/Tiled/TileSets/PlatformTracks.tsx
+++ Assets/Tiled/TileSets/PlatformTracks.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.11" tiledversion="1.11.2" name="PlatformTracks" tilewidth="32" tileheight="32" tilecount="256" columns="16" objectalignment="topleft" tilerendersize="grid" fillmode="preserve-aspect-fit">
- <image source="PlatformTracks.aseprite" width="512" height="512"/>
+<tileset version="1.11" tiledversion="1.11.2" name="PlatformTracks" tilewidth="32" tileheight="32" tilecount="256" columns="8" objectalignment="topleft" tilerendersize="grid" fillmode="preserve-aspect-fit">
+ <image source="PlatformTracks.aseprite" width="256" height="256"/>
  <tile id="0" type="Track">
   <properties>
    <property name="type" type="int" propertytype="TrackType" value="2"/>
@@ -36,29 +36,74 @@
    <property name="type" type="int" propertytype="TrackType" value="1"/>
   </properties>
  </tile>
+ <tile id="8" type="Track">
+  <properties>
+   <property name="type" type="int" propertytype="TrackType" value="0"/>
+  </properties>
+ </tile>
+ <tile id="10" type="Track">
+  <properties>
+   <property name="type" type="int" propertytype="TrackType" value="0"/>
+  </properties>
+ </tile>
+ <tile id="12" type="Track">
+  <properties>
+   <property name="type" type="int" propertytype="TrackType" value="0"/>
+  </properties>
+ </tile>
+ <tile id="13" type="Track">
+  <properties>
+   <property name="type" type="int" propertytype="TrackType" value="1"/>
+  </properties>
+ </tile>
+ <tile id="14" type="Track">
+  <properties>
+   <property name="type" type="int" propertytype="TrackType" value="1"/>
+  </properties>
+ </tile>
  <tile id="16" type="Track">
+  <properties>
+   <property name="type" type="int" propertytype="TrackType" value="2"/>
+  </properties>
+ </tile>
+ <tile id="17" type="Track">
   <properties>
    <property name="type" type="int" propertytype="TrackType" value="0"/>
   </properties>
  </tile>
  <tile id="18" type="Track">
   <properties>
+   <property name="type" type="int" propertytype="TrackType" value="2"/>
+  </properties>
+ </tile>
+ <tile id="19" type="Track">
+  <properties>
    <property name="type" type="int" propertytype="TrackType" value="0"/>
   </properties>
  </tile>
  <tile id="20" type="Track">
   <properties>
+   <property name="type" type="int" propertytype="TrackType" value="2"/>
+  </properties>
+ </tile>
+ <tile id="24" type="Track">
+  <properties>
    <property name="type" type="int" propertytype="TrackType" value="0"/>
   </properties>
  </tile>
- <tile id="21" type="Track">
+ <tile id="26" type="Track">
   <properties>
-   <property name="type" type="int" propertytype="TrackType" value="1"/>
+   <property name="type" type="int" propertytype="TrackType" value="0"/>
   </properties>
  </tile>
- <tile id="22" type="Track">
+ <tile id="28" type="Track">
   <properties>
-   <property name="type" type="int" propertytype="TrackType" value="1"/>
+   <property name="type" type="int" propertytype="TrackType" value="0"/>
+  </properties>
+ </tile>
+ <tile id="29" type="Track">
+  <properties>
+   <property name="type" type="int" propertytype="TrackType" value="0"/>
   </properties>
  </tile>
  <tile id="32" type="Track">
@@ -86,51 +131,6 @@
    <property name="type" type="int" propertytype="TrackType" value="2"/>
   </properties>
  </tile>
- <tile id="48" type="Track">
-  <properties>
-   <property name="type" type="int" propertytype="TrackType" value="0"/>
-  </properties>
- </tile>
- <tile id="50" type="Track">
-  <properties>
-   <property name="type" type="int" propertytype="TrackType" value="0"/>
-  </properties>
- </tile>
- <tile id="52" type="Track">
-  <properties>
-   <property name="type" type="int" propertytype="TrackType" value="0"/>
-  </properties>
- </tile>
- <tile id="53" type="Track">
-  <properties>
-   <property name="type" type="int" propertytype="TrackType" value="0"/>
-  </properties>
- </tile>
- <tile id="64" type="Track">
-  <properties>
-   <property name="type" type="int" propertytype="TrackType" value="2"/>
-  </properties>
- </tile>
- <tile id="65" type="Track">
-  <properties>
-   <property name="type" type="int" propertytype="TrackType" value="0"/>
-  </properties>
- </tile>
- <tile id="66" type="Track">
-  <properties>
-   <property name="type" type="int" propertytype="TrackType" value="2"/>
-  </properties>
- </tile>
- <tile id="67" type="Track">
-  <properties>
-   <property name="type" type="int" propertytype="TrackType" value="0"/>
-  </properties>
- </tile>
- <tile id="68" type="Track">
-  <properties>
-   <property name="type" type="int" propertytype="TrackType" value="2"/>
-  </properties>
- </tile>
  <wangsets>
   <wangset name="Tracks" type="edge" tile="4">
    <wangcolor name="Tracks" color="#ff0000" tile="4" probability="1"/>
@@ -141,24 +141,24 @@
    <wangtile tileid="4" wangid="0,0,0,0,1,0,1,0"/>
    <wangtile tileid="5" wangid="0,0,0,0,1,0,0,0"/>
    <wangtile tileid="6" wangid="1,0,0,0,0,0,0,0"/>
-   <wangtile tileid="16" wangid="1,0,0,0,1,0,0,0"/>
-   <wangtile tileid="18" wangid="1,0,0,0,1,0,0,0"/>
-   <wangtile tileid="20" wangid="1,0,0,0,1,0,0,0"/>
-   <wangtile tileid="21" wangid="0,0,0,0,0,0,1,0"/>
-   <wangtile tileid="22" wangid="0,0,1,0,0,0,0,0"/>
-   <wangtile tileid="32" wangid="1,0,1,0,1,0,0,0"/>
+   <wangtile tileid="8" wangid="1,0,0,0,1,0,0,0"/>
+   <wangtile tileid="10" wangid="1,0,0,0,1,0,0,0"/>
+   <wangtile tileid="12" wangid="1,0,0,0,1,0,0,0"/>
+   <wangtile tileid="13" wangid="0,0,0,0,0,0,1,0"/>
+   <wangtile tileid="14" wangid="0,0,1,0,0,0,0,0"/>
+   <wangtile tileid="16" wangid="1,0,1,0,1,0,0,0"/>
+   <wangtile tileid="17" wangid="0,0,1,0,0,0,1,0"/>
+   <wangtile tileid="18" wangid="1,0,1,0,1,0,1,0"/>
+   <wangtile tileid="19" wangid="0,0,1,0,0,0,1,0"/>
+   <wangtile tileid="20" wangid="1,0,0,0,1,0,1,0"/>
+   <wangtile tileid="24" wangid="1,0,0,0,1,0,0,0"/>
+   <wangtile tileid="26" wangid="1,0,0,0,1,0,0,0"/>
+   <wangtile tileid="28" wangid="1,0,0,0,1,0,0,0"/>
+   <wangtile tileid="32" wangid="1,0,1,0,0,0,0,0"/>
    <wangtile tileid="33" wangid="0,0,1,0,0,0,1,0"/>
-   <wangtile tileid="34" wangid="1,0,1,0,1,0,1,0"/>
+   <wangtile tileid="34" wangid="1,0,1,0,0,0,1,0"/>
    <wangtile tileid="35" wangid="0,0,1,0,0,0,1,0"/>
-   <wangtile tileid="36" wangid="1,0,0,0,1,0,1,0"/>
-   <wangtile tileid="48" wangid="1,0,0,0,1,0,0,0"/>
-   <wangtile tileid="50" wangid="1,0,0,0,1,0,0,0"/>
-   <wangtile tileid="52" wangid="1,0,0,0,1,0,0,0"/>
-   <wangtile tileid="64" wangid="1,0,1,0,0,0,0,0"/>
-   <wangtile tileid="65" wangid="0,0,1,0,0,0,1,0"/>
-   <wangtile tileid="66" wangid="1,0,1,0,0,0,1,0"/>
-   <wangtile tileid="67" wangid="0,0,1,0,0,0,1,0"/>
-   <wangtile tileid="68" wangid="1,0,0,0,0,0,1,0"/>
+   <wangtile tileid="36" wangid="1,0,0,0,0,0,1,0"/>
   </wangset>
  </wangsets>
 </tileset>
