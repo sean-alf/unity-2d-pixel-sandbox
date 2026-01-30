6f0bc0ab7d465a99e2f7ced2a2d36a72d84451bb
Sean Alf <no-email>
2026-01-30 15:44:00 -0800

    https://github.com/sean-alf/unity-2d-pixel-sandbox/issues/61

---
+++ Assets/Tiled/TileSets/GeneralFloorAndGround.tsx
@@ -0,0 +1,29 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.2" name="GeneralFloorAndGround" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="GeneralGroundAndFloor.aseprite" width="512" height="512"/>
+ <wangsets>
+  <wangset name="BrownGroundBorder" type="corner" tile="28">
+   <wangcolor name="BrownGroundBorder" color="#ff0000" tile="28" probability="1"/>
+   <wangtile tileid="11" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="12" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="27" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="28" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="41" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="42" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="43" wangid="0,1,0,1,0,1,0,0"/>
+   <wangtile tileid="44" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="45" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="46" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="57" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="58" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="59" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="60" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="61" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="62" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="75" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="76" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="91" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="92" wangid="0,0,0,0,0,0,0,1"/>
+  </wangset>
+ </wangsets>
+</tileset>
